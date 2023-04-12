39ee8947e9f533748bd8bcacb186719c1c5c6bfd
fakhruddinarif <muhfakhruddinarif30@gmail.com>
2023-04-12 10:43:15 +0700

    Update UTS

--- UTS/src/Mahasiswa.java
+++ UTS/src/Mahasiswa.java
@@ -1,10 +1,71 @@
 public class Mahasiswa {
-    String nama, nim;
-    String convert;
+    int numberName[] = new int[22];
+    int numberNim[] = new int[10];
+    int convert[] = new int[32];
 
-    Mahasiswa(String nama, String nim) {
-        this.nama = nama;
-        this.nim = nim;
-        convert = this.nim + this.nama;
+    void addName(int ...name) {
+        for (int i = 0; i < name.length; i++) {
+            numberName[i] = name[i];
+        }
+    }
+    void addNim(int ...nim) {
+        for (int i = 0; i < nim.length; i++) {
+            numberNim[i] = nim[i];
+        }
+    }
+
+    void convertData() {
+        for (int i = 0; i < numberNim.length; i++) {
+            convert[i] = numberNim[i];
+        }
+        for (int i = numberNim.length; i < numberName.length + numberNim.length; i++) {
+            convert[i] = numberName[i - numberNim.length];
+        }
+    }
+
+    void display() {
+        for (int i = 0; i < convert.length; i++) {
+            System.out.print(convert[i] + " | ");
+        }
+        System.out.println();
+    }
+
+    void bubbleSort() {
+        int temp[] = new int[convert.length];
+        for (int i = 0; i < convert.length - 1; i++) {
+            for (int j = 1; j < convert.length - i; j++) {
+                if (convert[j] < convert[j - 1]) {
+                    temp[j] = convert[j];
+                    convert[j] = convert[j - 1];
+                    convert[j - 1] = temp[j];
+                }
+            }
+        }
+    }
+
+    int FindBinarySearch(int search, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (search == convert[mid]) {
+                return (mid);
+            }
+            else if (convert[mid] > search) {
+                return FindBinarySearch(search, left, mid - 1);
+            }
+            else {
+                return FindBinarySearch(search, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+
+    void showPosition(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("Angka: " + x + " Pertama Kali Ditemukan Pada Index Ke-" + pos);
+        }
+        else {
+            System.out.println("Angka " + x + " Tidak Ditemukan");
+        }
     }
 }
--- UTS/src/Main.java
+++ UTS/src/Main.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Konversi konversi = new Konversi();
-        Mahasiswa mhs = new Mahasiswa("1321811313146111818214491411896", "2241720030");
-        konversi.add(mhs);
+        Scanner sc = new Scanner(System.in);
+        Mahasiswa mhs = new Mahasiswa();
 
+        mhs.addName(13, 21, 8, 1, 13, 13, 1, 4, 6, 1, 11, 8, 18, 21, 4, 4, 9, 14, 1, 18, 9, 6);
+        mhs.addNim(2, 2, 4, 1, 7, 2, 0, 0, 3, 0);
+        mhs.convertData();
+        System.out.println("Data Keseluruhan: ");
+        mhs.display();
+        mhs.bubbleSort();
+        System.out.println("Data Setelah Di Sorting: ");
+        mhs.display();
 
+        System.out.println("===============================================");
+        System.out.println("Pencarian Angka");
+        System.out.print("Masukkan Angka Yang Ingin Dicari: ");
+        int inputSearch = sc.nextInt();
+        int position = mhs.FindBinarySearch(inputSearch, 0, mhs.convert.length - 1);
+        mhs.showPosition(inputSearch, position);
     }
 }
